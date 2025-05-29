e52b5eb5782b5e35b3afc0d145c98a8d95ab1d83
Travis Irby <travis.irby@gmail.com>
2025-05-29 12:48:19 -0400

    tweaking vercel config approach

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/react-start/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    preset: 'vercel',
+  },
   vite: {
     plugins: [tailwindcss()],
   },
