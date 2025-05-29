6329746cb02e47817765f6c37fb61e11d42ec53f
Travis Irby <travis.irby@gmail.com>
2025-05-29 13:30:40 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+// With Tailwind CSS v4 and @tailwindcss/vite, this file might not be needed
+// as the Vite plugin handles PostCSS setup, including Tailwind and Autoprefixer.
+// If CSS issues persist after this change, try deleting this file.
 export default {
   plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
   },
 }
